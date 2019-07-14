76964beab9f2ee198cf5188aa5e89281cb70ceb0
Marvin <24806960+MagicMarvMan@users.noreply.github.com>
2019-07-14 18:13:08 +0200

    Create reserved.js

---
+++ reserved.js
@@ -0,0 +1,22 @@
+// This is the list of reserved domain names.
+// These can only be assigned if you have a good reason.
+// Please open an issue and explain the reason why you want the domain.
+// Those names marked with "blocked" cannot be assigned because they are system related.
+module.exports = [
+  "mail",
+  "admin",
+  "oss",
+  "get",
+  "save",
+  "learn",
+  "my",
+  "your",
+  "forum",
+  "community",
+  "autoconfig", // blocked
+  "mail", // blocked
+  "smtp", // blocked
+  "imap", // blocked
+  "pop", // blocked
+  "admin", // blocked
+]
